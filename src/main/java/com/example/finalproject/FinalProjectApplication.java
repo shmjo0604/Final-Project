2a54dc0859cad7e30dbec36763fe5bfb2a190547
LeeSeongHui <seonghui3472@gmail.com>
2023-05-18 01:37:57 +0900

    fragment

--- src/main/java/com/example/finalproject/FinalProjectApplication.java
+++ src/main/java/com/example/finalproject/FinalProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.controller"})
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
