93b45374868d101ed14f0c56be5fb5ee4731b180
LeeSeongHui <seonghui3472@gmail.com>
2023-05-18 17:27:54 +0900

    20230518

---
+++ src/main/java/com/example/controller/MypageController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MypageController {
+
+    @GetMapping(value="/mypage.do")
+    public String homeGET() {
+        return "mypage/mypage_main";
+    }
+    
+}
