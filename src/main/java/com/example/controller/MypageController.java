b0993aa5fd63fe0f7923c6ccf0c112d69de3a946
LeeSeongHui <seonghui3472@gmail.com>
2023-05-23 17:47:56 +0900

    20230523

--- src/main/java/com/example/controller/MypageController.java
+++ src/main/java/com/example/controller/MypageController.java
@@ -2,15 +2,22 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
+@RequestMapping(value = "/member")
+@RequiredArgsConstructor
 public class MypageController {
 
+    final String format = "MypageController => {}";
+
     @GetMapping(value="/mypage.do")
-    public String homeGET() {
+    public String myppageGET() {
         return "mypage/main";
     }
 
-
-    
 }
