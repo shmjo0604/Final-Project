8edcc6566551b2d9c04eb54c79eef7eadec98e81
LeeSeongHui <seonghui3472@gmail.com>
2023-05-29 10:06:03 +0900

    conflict modify

--- src/main/java/com/example/finalproject/FinalProjectApplication.java
+++ src/main/java/com/example/finalproject/FinalProjectApplication.java
@@ -1,11 +1,30 @@
 package com.example.finalproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.controller"})
+@PropertySource(value = {"classpath:global.properties"})
+@EntityScan(basePackages = {"com.example.entity"})
+@EnableJpaRepositories(basePackages = {"com.example.repository"})
+@MapperScan(basePackages = {"com.example.mapper"})
+@ComponentScan(basePackages = {
+	"com.example.controller",
+	"com.example.service",
+	"com.example.service.classproduct",
+	"com.example.service.member",
+	"com.example.service.apply",
+	"com.example.service.review",
+	"com.example.service.community",
+	"com.example.restcontroller",
+	"com.example.config",
+	"com.example.filter"
+})
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
