42bd76f1635bfa9aa1071af794e8f4cd7cafb600
LeeSeongHui <seonghui3472@gmail.com>
2023-05-30 17:55:45 +0900

    20230530

--- src/main/java/com/example/controller/ClassUnitController.java
+++ src/main/java/com/example/controller/ClassUnitController.java
@@ -1,10 +1,37 @@
 package com.example.controller;
-
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/classunit")
 public class ClassUnitController {
+
+    @GetMapping(value = "/myunit.do")
+    public String myunitGET(
+        @RequestParam(name = "menu", defaultValue = "0") int menu,
+        @AuthenticationPrincipal User user,
+        Model model) {
+
+        String id = user.getUsername();
+
+        if(menu == 0){
+            return "redirect:/classunit/myunit.do?menu=1";
+        }
+
+        if(menu == 1){
+
+        }
+
+        else if(menu == 2){
+
+        }
+
+        return "/classunit/unit";
+    }
     
 }
