368b9a48cf695d35008b12616fd36926f4f9c1e5
shmjo0604 <shmjo0604@gmail.com>
2023-06-14 17:25:52 +0900

    23.06.14
    23.06.14 추가 보완사항

--- src/main/java/com/example/repository/NotificationRepository.java
+++ src/main/java/com/example/repository/NotificationRepository.java
@@ -13,7 +13,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findTop10ByMember_idOrderByNoDesc(String id);
 
     @Query(
-        value="SELECT * FROM (SELECT n.*, ROW_NUMBER() OVER(ORDER BY n.no DESC) rnum FROM NOTIFICATION n WHERE n.receiver = :id ) n WHERE rnum BETWEEN 1 AND 10 ORDER BY n.rnum ASC ", 
+        value="SELECT * FROM (SELECT n.*, ROW_NUMBER() OVER(ORDER BY n.no DESC) rnum FROM NOTIFICATION n WHERE n.receiver = :id ) n WHERE rnum BETWEEN 1 AND 6 ORDER BY n.rnum ASC ", 
         nativeQuery = true) // :name (nativequery) = #{name} (mybatis) 
     List<Notification> selectByMemberidOrderByNoDescLimit10(@Param("id") String id);
 
